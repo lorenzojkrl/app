43a474787fb4127792675f10321b2f1bc12cf1f0
Alessandro Carnelli <aleilkappa@msn.com>
2020-12-14 14:59:18 +0100

    agiunto header

--- screens/Login.js
+++ screens/Login.js
@@ -3,12 +3,14 @@ import { View, StyleSheet, Text } from 'react-native'
 import Title from '../components/Title'
 import Label from '../components/Label'
 import Input from '../components/Input'
+import Header from '../components/Header'
 
 
 export default function Login() {
     return (
         <>
             <View style={styles.loginSpace}>
+                <Header><Text>Nome App</Text></Header>
                 <Title title={'Accedi'}></Title>
                 <Label label={'Nome utente / Email'} />
                 <Input />
@@ -26,5 +28,6 @@ const styles = StyleSheet.create({
     loginSpace: {
         flex: 1,
         alignItems: 'center',
+        justifyContent: 'center',
     }
 })
