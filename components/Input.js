802a024683e3df76ed328d1a6a4e22fd51e4825a
Alessandro Carnelli <aleilkappa@msn.com>
2020-12-14 12:10:30 +0100

    prova

--- components/Input.js
+++ components/Input.js
@@ -1,5 +1,19 @@
 import React from "react";
-
+import {
+    Text,
+    View,
+    TextInput,
+    StyleSheet
+} from 'react-native'
 export default function Input(){
-    return <></>
+    const [value, setValue] = useState("")
+
+    return (
+        <View style={styles.textArea}>
+
+            <TextInput
+                onChangeText={text => setValue(text)}
+                value={value} />
+        </View>
+    )
 }
