f17d3d28856c00a95ca2709dc198d95a75a1054f
Lorenzo <lorenzozar@gmail.com>
2020-12-16 11:59:26 +0200

    comment

--- navigators/Main.js
+++ navigators/Main.js
@@ -4,7 +4,7 @@ import Header from '../components/Header'
 import Spacer from '../components/Spacer'
 import Button from '../components/Button'
 import api from '../utility/api'
-// import { EvilIcons } from '@expo/vector-icons';
+import { EvilIcons } from '@expo/vector-icons';
 
 
 // usare createBottommTabNavigator: https://reactnavigation.org/docs/bottom-tab-navigator/
@@ -35,16 +35,13 @@ export default function Main() {
             <Header>
                 <Text>Nome Main  </Text>
                 <Text>Nome</Text>
-                {/* <EvilIcons name="user" size={36} color="black" style={{ backgroundColor: "yellow", borderRadius: 30, borderWidth: 2, overflow: "hidden" }} /> */}
+                <EvilIcons name="user" size={50} color="yellow" />
             </Header>
             <Spacer size={30} />
             <Button
                 name={'CARTE'}
                 submit={() => submitGet()}
             />
-
-
-
         </View>
     )
 }
